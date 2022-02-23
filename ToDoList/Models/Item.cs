5e6539bbcd831946d2c688a464e1131c96eb9503
Ryan Ashby <rjapsu160@gmail.com>
2022-02-23 08:42:35 -0800

    initial commit for To Do List

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,22 @@
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public string Description { get; set ;}
+    private static List<Item> _instances = new List<Item>{};
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public Item (string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
+  }
+}
