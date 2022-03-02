16f8430353ce8abf30bd932c32064d164e5d8aa6
Ryan Ashby <rjapsu160@gmail.com>
2022-03-02 09:55:56 -0800

    second lesson test good fail
    Co-Authored-By: Mikah Mathews <mikah@outofband.org>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -89,10 +89,26 @@ namespace ToDoList.Tests
       Item newItem = new Item(description);
 
       //Act
-      int result = 0;
+      int result = newItem.Id;
 
       //Assert
       Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+
+      //Act
+      Item result = new Item("Incorrect test item");
+
+      //Assert
+      Assert.AreEqual(newItem2, result);
     }
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,12 +5,14 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
+    public int Id { get; }
     private static List<Item> _instances = new List<Item> {};
 
     public Item (string description)
     {
       Description = description;
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public static List<Item> GetAll()
