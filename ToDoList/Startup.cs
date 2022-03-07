6ba6d10f891c2c63483caa7df6cb950a12ee0d6f
Ryan Ashby <rjapsu160@gmail.com>
2022-03-07 14:32:15 -0800

    updating ToDoList with entity framework

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
 
 namespace ToDoList
 {
@@ -11,16 +13,19 @@ namespace ToDoList
     public Startup(IWebHostEnvironment env)
     {
       var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddEnvironmentVariables();
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json");
       Configuration = builder.Build();
     }
 
-    public IConfigurationRoot Configuration { get; }
+    public IConfigurationRoot Configuration { get; set; }
 
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
+      services.AddEntityFrameworkMySql()
+        .AddDbContext<ToDoListContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
     public void Configure(IApplicationBuilder app)
@@ -39,9 +44,5 @@ namespace ToDoList
         await context.Response.WriteAsync("Hello World!");
       });
     }
-  }
-  public static class DBConfiguration
-  {
-    public static string ConnectionString = "server=localhost;user id=root;password=Qwer2468!!;port=3306;database=to_do_list;";
   }
 }
